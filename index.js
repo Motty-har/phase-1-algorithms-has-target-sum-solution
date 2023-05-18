9bd28b926565b65c9e8d0b40937569c3898cc6a3
Motty-har <harligmotty@gmail.com>
2023-05-17 21:03:44 -0400

    Finished

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  const seenNumbers = {}
+  for(let number of array){
+    const complement = target - number
+    if(seenNumbers[complement])return true
+
+    seenNumbers[number] = true
+  }
+  return false
 }
 
 /* 
-  Write the Big O time complexity of your function here
+need to create a function that takes in two arguments, an array and a target number.
+if two of the elements combined = the target number - return as true. else return false  
+
+
+
+
+Write the Big O time complexity of your function here
+o(n)
 */
 
 /* 
   Add your pseudocode here
+  create an empty object to hold the elements of the array we iterated through
+  for current number, identify a complementry number(ex. element is 5, target is 25, subtract 5 from 25, complementry number is 20)
+  look through oject to see if it has the complementry number 
+  if yes = true
+  if not keep iteratinf till the end and return false if no complementry numbers are found 
 */
 
 /*
